1546c7b65e24c6c14ba23f889d1e76168a27f71f
Dongdong Xu <dongdong.xu1@thoughtworks.com>
2023-01-08 21:35:01 +0800

    fix buffer issue for browsers, because Webpack 5 no longer polyfills Node.js core modules automatically

--- build/webpack.web.js
+++ build/webpack.web.js
@@ -2,5 +2,12 @@ module.exports = {
     target: 'web',
     output: {
         filename: 'web.js'
-    }
+    },
+    // NOTE: https://webpack.js.org/configuration/resolve/#resolvefallback
+    resolve: {
+        extensions: ['.ts', '.js'],
+        fallback: {
+          buffer: require.resolve('buffer')
+      },
+    },
 };
